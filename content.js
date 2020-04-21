ddbf92d9835dcbe8a000ed17e89198eeba1ae916
Eduard Florea <edward.florea@gmail.com>
2020-04-21 12:24:27 -0700

    save mapping based on week

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 let timesheet = [];
+let startOfWeek;
+let storage_key;
 
-const parseTimesheet = function( timeEntries ) {
+const parseTimesheet = function( timeEntries, weekStart ) {
     // reset everything.
     [].forEach.call(document.querySelectorAll('.employee-client-hours-logged'),function(e){
         e.parentNode.removeChild(e);
@@ -12,16 +14,16 @@ const parseTimesheet = function( timeEntries ) {
     } else {
         timesheet = timeEntries;
     }
-    console.log( '----------timeentries-------' );
-    console.log( timeEntries );
-    console.log( '-----------------------------------' );
+    startOfWeek = weekStart;
+    storage_key = 'mapping-' + startOfWeek;
     let totalHoursLogged = 0;
+    let totalHoursMapped = 0;
     timesheet.forEach( function( p ) {
         totalHoursLogged += p.hours;
     } );
 
-    chrome.storage.sync.get( {
-        mapping: false,
+    chrome.storage.local.get( {
+        [storage_key]: false,
     }, function( items ) {
 
         resourcing.forEach( function( el, index )  {
@@ -40,15 +42,15 @@ const parseTimesheet = function( timeEntries ) {
                         } else {
                             // check mapping.
                             const current_project = p;
-                            if ( items.mapping['project[' + project_name.replace( '&amp;', '&' ) + '][]'] ) {
-                                if ( Array.isArray( items.mapping['project[' + project_name.replace( '&amp;', '&' ) + '][]'] ) ) {
-                                    items.mapping['project[' + project_name.replace( '&amp;', '&' ) + '][]'].forEach( function( map_item ) {
+                            if ( items[storage_key]['project[' + project_name.replace( '&amp;', '&' ) + '][]'] ) {
+                                if ( Array.isArray( items[storage_key]['project[' + project_name.replace( '&amp;', '&' ) + '][]'] ) ) {
+                                    items[storage_key]['project[' + project_name.replace( '&amp;', '&' ) + '][]'].forEach( function( map_item ) {
                                         if ( map_item === 'cp' + current_project.client_id + '-' + current_project.project_id ) {
                                             hours_logged = hours_logged + parseFloat( current_project.hours );
                                         }
                                     } );
                                 } else {
-                                    if( items.mapping['project[' + project_name.replace( '&amp;', '&' ) + '][]'] === 'cp' + current_project.client_id + '-' + current_project.project_id ) {
+                                    if( items[storage_key]['project[' + project_name.replace( '&amp;', '&' ) + '][]'] === 'cp' + current_project.client_id + '-' + current_project.project_id ) {
                                         hours_logged = hours_logged + parseFloat( current_project.hours );
                                     }
                                 }
@@ -56,6 +58,15 @@ const parseTimesheet = function( timeEntries ) {
                             }
                         }
                     } );
+                }
+                // check if last row.
+                if ( ! resourcing[index+1] ) {
+                    hours_logged = totalHoursLogged;
+                    if ( hours_logged != totalHoursMapped ) {
+                        hours_logged = hours_logged + ' mapped: ' + totalHoursMapped;
+                    }
+                } else {
+                    totalHoursMapped += hours_logged;
                 }
                 el.insertAdjacentHTML( 'beforeend', '<div class="employee-cell employee-client-hours-logged">' + hours_logged + '</div>' );
             }
@@ -66,12 +77,12 @@ const parseTimesheet = function( timeEntries ) {
 const mapEntries = function( e ) {
     e.preventDefault();
 
-    chrome.storage.sync.get( {
-        mapping: {}
+    chrome.storage.local.get( {
+        [storage_key]: {}
     }, function( items ) {
 
         let resourcing = document.querySelectorAll( '.employee-schedule-row' );
-        let output = '<div style="padding: 1rem; background: #000; color: #FFF; position: absolute; top:0; left: 0; width: 500px; height: 500px; z-index: 99999; border: 1px solid red; overflow: auto;"><form id="form_mapping">';
+        let output = '<div id="mapping_content"><form id="form_mapping">';
         let timesheet_dropdown_options = '';
         timesheet.forEach( function( p ) {
             timesheet_dropdown_options += '<option value="cp' + p.client_id + '-' + p.project_id + '">' + p.project.replace( '&amp;', '&' ) + '</option>';
@@ -81,19 +92,19 @@ const mapEntries = function( e ) {
             const project = el.querySelector( '.employee-client-project' );
             if ( project ) {
                 const project_name = project.innerHTML.replace( '&amp;', '&' );
-                output += '<div><label>' + project_name + ':</label><br><select style="color: #000;" name="project[' + project_name + '][]" multiple size="5">';
+                output += '<div><label>' + project_name + ':</label><select name="project[' + project_name + '][]" multiple size="5">';
                 timesheet.forEach( function( p ) {
                     output += '<option value="cp' + p.client_id + '-' + p.project_id + '"';
                     // check mapping.
-                    if ( items.mapping['project[' + project_name + '][]'] ) {
-                        if ( Array.isArray( items.mapping['project[' + project_name + '][]'] ) ) {
-                            items.mapping['project[' + project_name + '][]'].forEach(function (map_item) {
+                    if ( items[storage_key]['project[' + project_name + '][]'] ) {
+                        if ( Array.isArray( items[storage_key]['project[' + project_name + '][]'] ) ) {
+                            items[storage_key]['project[' + project_name + '][]'].forEach(function (map_item) {
                                 if ( map_item === 'cp' + p.client_id + '-' + p.project_id ) {
                                     output += ' selected';
                                 }
                             });
                         } else {
-                            if( items.mapping['project[' + project_name + '][]'] === 'cp' + p.client_id + '-' + p.project_id ) {
+                            if( items[storage_key]['project[' + project_name + '][]'] === 'cp' + p.client_id + '-' + p.project_id ) {
                                 output += ' selected';
                             }
                         }
@@ -103,7 +114,7 @@ const mapEntries = function( e ) {
                 output += '</select></div><br>';
             }
         } );
-        output += '<input type="submit" style="color: #000;" value="Save mapping"></form></div>';
+        output += '<input type="submit" value="Save mapping"></form></div>';
 
         document.getElementById( 'mapping' ).insertAdjacentHTML('afterEnd', output );
 
@@ -123,10 +134,10 @@ const mapEntries = function( e ) {
                 }
                 object[key].push(value);
             });
-           chrome.storage.sync.set( {
-               mapping: object
+           chrome.storage.local.set( {
+               [storage_key]: object
            }, function( items ) {
-               parseTimesheet( timesheet );
+               parseTimesheet( timesheet, startOfWeek );
            } );
             return false;
         } );
--- timesheet.js
+++ timesheet.js
@@ -3,8 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const dashboardURL = 'https://dashboard.10up.com/blog/10upper/';
     let dashboardId, harvestId, harvestApiKey;
     var timesheet = {};
+    let startOfWeek = '';
 
-    chrome.storage.sync.get({
+    chrome.storage.local.get({
         dashboardId: '',
         harvestId: '',
         harvestApiKey: ''
@@ -20,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
         dashboardId = document.getElementById('dashboard-id').value;
         harvestId = document.getElementById('harvest-id').value;
         harvestApiKey = document.getElementById('harvest-api-key').value;
-        chrome.storage.sync.set({
+        chrome.storage.local.set({
             dashboardId: dashboardId,
             harvestId: harvestId,
             harvestApiKey: harvestApiKey,
@@ -34,17 +35,28 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     };
 
+    const setToMonday = function( date ) {
+        let day = date.getDay() || 7;
+        console.log( day );
+        if ( day === 6 ) {
+            date.setHours( 24 );
+        } else {
+            if ( day !== 1 && day !== 7 ) {
+                date.setHours(-24 * (day - 1));
+            }
+        }
+        console.log( date );
+        return date;
+    };
+
+    const formatDate = function( date ) {
+         return date.getFullYear() + '-' + ( ( date.getMonth() + 1 ) < 10 ? '0' : '' ) + ( date.getMonth() + 1 ) + '-' + ( ( date.getDate() < 10 ) ? '0' : '' ) + date.getDate();
+    };
+
     const getTimesheet = function() {
 
-        var curr = new Date(); // get current date
-        var first = curr.getDate() - curr.getDay() + 1; // First day is the day of the month - the day of the week
-        var last = first + 6; // last day is the first day + 6
-
-        var firstday = new Date(curr.setDate(first));
-        var lastday = new Date(curr.setDate(last));
-
-        const startDate = firstday.getFullYear() + '-' + ( ( firstday.getMonth() + 1 ) < 10 ? '0' : '' ) + ( firstday.getMonth() + 1 ) + '-' + ( ( firstday.getDate() < 10 ) ? '0' : '' ) + firstday.getDate();
-        const endDate   = lastday.getFullYear() + '-' + ( ( lastday.getMonth() + 1 ) < 10 ? '0' : '' ) + ( lastday.getMonth() + 1 ) + '-' + ( ( lastday.getDate() < 10 ) ? '0' : '' ) + lastday.getDate();
+        const startDate = moment( startOfWeek ).startOf('isoWeek').format( "YYYY-MM-DD" )
+        const endDate   = moment( startOfWeek ).add(1, 'weeks').startOf('week').format( "YYYY-MM-DD" )
 
         var xhr = new XMLHttpRequest();
         xhr.withCredentials = true;
@@ -68,8 +80,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
                     const stringTimesheet = JSON.stringify( timesheet );
+                    const weekStart = JSON.stringify( startOfWeek );
                     chrome.tabs.executeScript( tabs[0].id, {
-                        'code': `parseTimesheet(${stringTimesheet})`
+                        'code': `parseTimesheet(${stringTimesheet},${weekStart})`
                     } );
                 } );
             }
@@ -82,19 +95,51 @@ document.addEventListener('DOMContentLoaded', function() {
         xhr.send();
     }
 
-    const checkDashboard = function() {
-        chrome.tabs.query( {
-            'active': true,
-            'currentWindow': true
-        }, function( tabs ) {
-            const regex = new RegExp( dashboardURL );
-            if( ! regex.test( tabs[0].url ) ) {
-                chrome.tabs.create( { 'url': dashboardURL + dashboardId + '/' } );
-            }
-        } );
+    const checkDashboard = function( checkTimesheet ) {
+
+        if ( checkTimesheet ) {
+            chrome.tabs.query({
+                'active': true,
+                'currentWindow': true
+            }, function (tabs) {
+                const url = new URL(tabs[0].url);
+                const arg = url.searchParams.get('week');
+                if (arg) {
+                    startOfWeek = arg;
+                } else {
+                    startOfWeek = moment().format( "YYYY-MM-DD" );
+                }
+
+                if( 0 === moment( startOfWeek ).day() ) {
+                    startOfWeek = moment(startOfWeek).add( 1, 'days' ).format( 'YYYY-MM-DD' );
+                }
+
+                getTimesheet();
+            });
+        }
+
+        if ( dashboardId ) {
+            chrome.tabs.query({
+                'active': true,
+                'currentWindow': true
+            }, function (tabs) {
+                const regex = new RegExp(dashboardURL);
+                if (!regex.test(tabs[0].url)) {
+                    chrome.tabs.create({'url': dashboardURL + dashboardId + '/'});
+                }
+            });
+        } else {
+            document.getElementById( 'tab-2' ).setAttribute( 'checked', 'checked' );
+        }
+
+        document.getElementById('dashboard-id').value    = dashboardId;
+        document.getElementById('harvest-id').value      = harvestId;
+        document.getElementById('harvest-api-key').value = harvestApiKey;
     };
 
     document.getElementById( 'save' ).addEventListener( 'click', saveOptions );
-    document.getElementById( 'dashboard-button' ).addEventListener( 'click', getTimesheet );
+    document.getElementById( 'dashboard-button' ).addEventListener( 'click', function() {
+        checkDashboard( true );
+    } );
 
 }, false);
